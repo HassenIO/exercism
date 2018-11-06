5bc446ae569a364934985afeae4bec2071e8a6ec
Hassen Taidirt <htaidirt@gmail.com>
2018-11-06 21:29:23 +0100

    Hello world in Go

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,11 @@
+/*
+Package greeting is a namespace for greetings code.
+This is for Hello World Exercism exercise.
+*/
+package greeting
+
+// HelloWorld is a function that just returns Hello, World! string
+// HelloWorld() string
+func HelloWorld() string {
+	return "Hello, World!"
+}
